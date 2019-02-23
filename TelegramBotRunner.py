f8ba517cc3cf3cccba9137fc223ea0564cba8772
logicalpsycho <37990015+logicalpsycho@users.noreply.github.com>
2019-02-23 14:17:11 +0900

    aleady had

--- TelegramBotRunner.py
+++ TelegramBotRunner.py
@@ -1,10 +1,49 @@
-import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import os
+import random
 
-my_token = '746952122:AAHbdl3fPpNJTnAWnX-QYCOSD3czGG4sSzI'
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-bot = telegram.Bot(token= my_token)
-updates = bot.getUpdates()
+logger = logging.getLogger(__name__)
 
-chat_id = bot.getUpdates()[-1].message.chat.id #
+def start(bot, update):
+    chat_id = update.message.chat_id
+    user = update.message.from_user
+    msg = "hi, %s %s! your user_id is %s, currently there is no urls now" %(user.first_name, user.last_name,user.id)
+    bot.sendMessage(update.message.chat_id, text=msg)
 
-bot.sendMessage(chat_id = chat_id, text="I am conan, It's a detective.")
+
+def query(msg) :
+  return "/help maybe help you ;)"
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text='/start : receive signin URL')
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+def response(bot, update):
+    chat_id = update.message.chat_id
+    user = update.message.from_user
+    user_name = "%s%s" %(user.last_name, user.first_name)
+    r_msg = query(update.message.text)
+    bot.sendMessage(chat_id, text=r_msg)
+
+def main():
+    token = '746952122:AAHbdl3fPpNJTnAWnX-QYCOSD3czGG4sSzI'
+    chatid= 746952122
+
+    updater = Updater(token)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("help",help))
+    dp.add_handler(CommandHandler("start", start))
+
+    dp.add_handler(MessageHandler([Filters.text],response))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
